7767e54824014b5b65abf65aa18c1df49dbf7f9a
Hermann EHO <hermannleboss@gmail.com>
2023-05-25 19:14:13 +0000

    feat: first version onSnapshot working

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,21 @@
 <script setup lang="ts">
+import { query, doc, collection, onSnapshot } from "firebase/firestore";
+import {db} from "./firebase"
+
+const q = query(collection(db, "teams"));
+const unsubscribe = onSnapshot(q, (querySnapshot) => {
+    const teams = [];
+    querySnapshot.forEach((doc) => {
+        teams.push(doc.data());
+    });
+    console.log("Current teams : ", teams);
+});
+
+
 </script>
 
 <template>
   <div><h1>Welcomme</h1></div>
 </template>
 
-<style scoped>
-</style>
+<style scoped></style>
