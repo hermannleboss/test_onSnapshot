00f2098475a0347fb7de6b0dfe6c7a12238e4401
Hermann EHO <hermannleboss@gmail.com>
2023-05-26 22:03:57 +0000

    feat: use on snapchot for store reactivity.

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,21 @@
 <script setup lang="ts">
-import {query, doc, collection, onSnapshot} from "firebase/firestore";
-import {db} from "./firebase"
-import {ref} from "vue";
-
-const q = query(collection(db, "teams"));
-const refTeams = ref()
-const loading = ref(false)
-const lastLoadingTime = ref(0)
-const unsubscribe = onSnapshot(q, (querySnapshot) => {
-    let start =  performance.now();
-    loading.value = true
-    const teams = [];
-    querySnapshot.forEach((doc) => {
-        teams.push(doc.data());
-    });
-    refTeams.value = teams
-    loading.value = false
-    lastLoadingTime.value = performance.now() - start;
-});
-
-
+import { useTeamStore } from '@/stores/team'
+const teamStore = useTeamStore()
 </script>
 
 <template>
-    <div><h1>Welcomme</h1>
-        <div>
-            <p> loading time={{lastLoadingTime}}</p>
-            <p>is loading = {{loading}}</p>
-            <p v-if="refTeams">teams {{refTeams.length}}</p>
-        </div>
+  <div>
+    <h1>Welcomme</h1>
+    <div v-if="teamStore.teams">
+      <p>teams {{ teamStore.teams?.length }}</p>
+      <div v-for="team in teamStore.teams" :key="team.id">
+        {{ team }}
+          <br>
+          <br>
+          <br>
+      </div>
     </div>
+  </div>
 </template>
 
 <style scoped></style>
---
+++ src/stores/team.ts
@@ -0,0 +1,19 @@
+import { ref, computed } from 'vue'
+import type { Ref } from 'vue'
+import { defineStore } from 'pinia'
+import { collection, onSnapshot, query } from 'firebase/firestore'
+import { db } from '@/firebase'
+
+export const useTeamStore = defineStore('team', () => {
+  const teams: Ref<undefined | Array<any>> = ref()
+  const q = query(collection(db, 'teams'))
+  const unsubscribe = onSnapshot(q, (querySnapshot) => {
+    const localTeams: any[] = []
+    querySnapshot.forEach((doc) => {
+      localTeams.push(doc.data())
+    })
+    teams.value = localTeams
+  })
+
+  return {teams}
+})
