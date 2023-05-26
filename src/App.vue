ba4c1c2c055058624b2c827967a96af045a477ee
Hermann EHO <hermannleboss@gmail.com>
2023-05-26 19:44:23 +0000

    feat: handle loading

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,35 @@
 <script setup lang="ts">
-import { query, doc, collection, onSnapshot } from "firebase/firestore";
+import {query, doc, collection, onSnapshot} from "firebase/firestore";
 import {db} from "./firebase"
+import {ref} from "vue";
 
 const q = query(collection(db, "teams"));
+const refTeams = ref()
+const loading = ref(false)
+const lastLoadingTime = ref(0)
 const unsubscribe = onSnapshot(q, (querySnapshot) => {
+    let start =  performance.now();
+    loading.value = true
     const teams = [];
     querySnapshot.forEach((doc) => {
         teams.push(doc.data());
     });
-    console.log("Current teams : ", teams);
+    refTeams.value = teams
+    loading.value = false
+    lastLoadingTime.value = performance.now() - start;
 });
 
 
 </script>
 
 <template>
-  <div><h1>Welcomme</h1></div>
+    <div><h1>Welcomme</h1>
+        <div>
+            <p> loading time={{lastLoadingTime}}</p>
+            <p>is loading = {{loading}}</p>
+            <p v-if="refTeams">teams {{refTeams.length}}</p>
+        </div>
+    </div>
 </template>
 
 <style scoped></style>
