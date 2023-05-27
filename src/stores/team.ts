aa1a0a7707a67d383bb9b3dd6cf180e36765d284
Hermann EHO <hermannleboss@gmail.com>
2023-05-27 11:41:23 +0000

    feat: refacto content

--- src/stores/team.ts
+++ src/stores/team.ts
@@ -1,4 +1,4 @@
-import { ref, computed } from 'vue'
+import { ref } from 'vue'
 import type { Ref } from 'vue'
 import { defineStore } from 'pinia'
 import { collection, onSnapshot, query } from 'firebase/firestore'
@@ -7,13 +7,15 @@ import { db } from '@/firebase'
 export const useTeamStore = defineStore('team', () => {
   const teams: Ref<undefined | Array<any>> = ref()
   const q = query(collection(db, 'teams'))
-  const unsubscribe = onSnapshot(q, (querySnapshot) => {
+   onSnapshot(q, (querySnapshot) => {
     const localTeams: any[] = []
     querySnapshot.forEach((doc) => {
       localTeams.push(doc.data())
     })
     teams.value = localTeams
   })
+
+
 
   return {teams}
 })
