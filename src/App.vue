66a77de91abed7fbf7510329e2a33a69b0081a18
Hermann EHO <hermannleboss@gmail.com>
2023-05-27 11:57:24 +0000

    feat: manage loading one by one

--- src/App.vue
+++ src/App.vue
@@ -6,14 +6,30 @@ const teamStore = useTeamStore()
 <template>
   <div>
     <h1>Welcomme</h1>
+    <div>current{{ teamStore.currentTeam }}</div>
+      <div  v-if="teamStore.teams==undefined">
+          Loading
+      </div>
     <div v-if="teamStore.teams">
       <p>teams {{ teamStore.teams?.length }}</p>
       <div v-for="team in teamStore.teams" :key="team.id">
-          {{ team.name }}
-          <br>
-          <p v-if="team.members">{{ team.members.length}}</p>
-          <br>
-          <br>
+        {{ team.name }}
+        <br />
+        <p v-if="team.members">{{ team.members.length }}</p>
+        <br />
+        <br />
+
+        <div>
+          <a
+            @click="
+              () => {
+                console.log('clicked', team.uid)
+                teamStore.fetchById(team.uid)
+              }
+            "
+            >Set current for {{ team.uid }}</a
+          >
+        </div>
       </div>
     </div>
   </div>
--- src/stores/team.ts
+++ src/stores/team.ts
@@ -1,11 +1,13 @@
 import { ref } from 'vue'
 import type { Ref } from 'vue'
 import { defineStore } from 'pinia'
-import { collection, onSnapshot, query } from 'firebase/firestore'
+import { collection, onSnapshot, query , doc} from 'firebase/firestore'
 import { db } from '@/firebase'
 
 export const useTeamStore = defineStore('team', () => {
   const teams: Ref<undefined | Array<any>> = ref()
+    const currentTeam = ref()
+    const currentSub=ref()
   const q = query(collection(db, 'teams'))
    onSnapshot(q, (querySnapshot) => {
     const localTeams: any[] = []
@@ -16,6 +18,17 @@ export const useTeamStore = defineStore('team', () => {
   })
 
 
+    const fetchById = (id: string) =>{
+      // if(currentSub.value){
+      //     currentSub.value()
+      // }
+        console.log("called", id)
+        const unsub = onSnapshot(doc(db, "teams", id), (doc) => {
+            console.log("called data", doc.data())
+            currentTeam.value=doc.data()
+        });
+        currentSub.value=unsub
+    }
 
-  return {teams}
+  return {teams, fetchById, currentTeam}
 })
