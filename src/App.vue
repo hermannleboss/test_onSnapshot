a891321a8e5cad8e5a6a354ce6fe806d8ff26503
Hermann EHO <hermannleboss@gmail.com>
2023-06-10 10:38:34 +0000

    feat: setup unsubscribe

--- src/App.vue
+++ src/App.vue
@@ -6,31 +6,30 @@ const teamStore = useTeamStore()
 <template>
   <div>
     <h1>Welcomme</h1>
-    <div>current{{ teamStore.currentTeam }}</div>
+    <div>current <span v-if="teamStore.currentTeam && teamStore.currentTeam.data">{{ teamStore.currentTeam.data.name }}</span></div>
       <div  v-if="teamStore.teams==undefined">
           Loading
       </div>
     <div v-if="teamStore.teams">
       <p>teams {{ teamStore.teams?.length }}</p>
       <div v-for="(team, index) in teamStore.teams" :key="team.id">
+          <br />
+          <br />
+          <div>
+              <a
+                  @click="
+              () => {
+                teamStore.fetchById(team.uid)
+              }
+            "
+              >Set current for <em>{{ team.name }}</em></a
+              >
+          </div>
         {{ team.name }}
         <br />
           counter {{index}}
         <p v-if="team.members">{{ team.members.length }}</p>
-        <br />
-        <br />
 
-        <div>
-          <a
-            @click="
-              () => {
-                console.log('clicked', team.uid)
-                teamStore.fetchById(team.uid)
-              }
-            "
-            >Set current for {{ team.uid }}</a
-          >
-        </div>
       </div>
     </div>
   </div>
--- src/stores/team.ts
+++ src/stores/team.ts
@@ -1,55 +1,54 @@
 import {ref, watchEffect} from 'vue'
-import type { Ref } from 'vue'
-import { defineStore } from 'pinia'
-import { collection, onSnapshot, query, doc } from 'firebase/firestore'
-import { db } from '@/firebase'
+import type {Ref} from 'vue'
+import {defineStore} from 'pinia'
+import {collection, onSnapshot, query, doc} from 'firebase/firestore'
+import {db} from '@/firebase'
 
 export const useTeamStore = defineStore('team', () => {
-  const teams: Ref<undefined | Array<any>> = ref()
-  const currentTeam = ref()
-  const currentSub = ref()
-  const q = query(collection(db, 'teams'))
+    const teams: Ref<undefined | Array<any>> = ref()
+    const currentTeam = ref()
+    const q = query(collection(db, 'teams'))
 
-  onSnapshot(q, (querySnapshot) => {
-    const localTeams: any[] = []
-    querySnapshot.forEach((doc) => {
-      localTeams.push(doc.data())
+    onSnapshot(q, (querySnapshot) => {
+        const localTeams: any[] = []
+        querySnapshot.forEach((doc) => {
+            localTeams.push(doc.data())
+        })
+        teams.value = localTeams
     })
-    teams.value = localTeams
-  })
 
-  watchEffect(()=>{
-    // Logs when the team is updated
-    console.log("teams",teams)
-  })
-  const fetchById = (id: string) => {
-    /**
-     * check if a subscription exist and unsubscribe it.
-     *
-     * Why it necessary
-     *
-     * If we fetch the team with id 1
-     * Then we call the team with id 2
-     * The current team will be the content of team 2
-     * Then if the content of team 1 is updated by someone else or another process
-     * The current will be updated to team 1.
-     * To avoid this effect we need to unsubscribe to old document update
-     *
-     */
-    if (currentSub.value) {
-      currentSub.value()
+    watchEffect(() => {
+        // Logs when the team is updated
+        // console.log("teams",teams)
+    })
+    const fetchById = (id: string) => {
+        /**
+         * check if a subscription exist and unsubscribe it.
+         *
+         * Why it necessary
+         *
+         * If we fetch the team with id 1
+         * Then we call the team with id 2
+         * The current team will be the content of team 2
+         * Then if the content of team 1 is updated by someone else or another process
+         * The current will be updated to team 1.
+         * To avoid this effect we need to unsubscribe to old document update
+         *
+         */
+        if (currentTeam.value && currentTeam.value.subscription) {
+            currentTeam.value.subscription()
+        }
+
+        // Create new subscription
+        const unsub = onSnapshot(doc(db, 'teams', id), (doc) => {
+
+            // Set team data value
+            currentTeam.value = {...currentTeam.value, data: doc.data()}
+        })
+
+        // Update the subscription
+        currentTeam.value = {...currentTeam.value, subscription: unsub}
     }
 
-    // Create new subscription
-    const unsub = onSnapshot(doc(db, 'teams', id), (doc) => {
-
-      // Set team data value
-      currentTeam.value = doc.data()
-    })
-
-    // Update the subscription
-    currentSub.value = unsub
-  }
-
-  return { teams, fetchById, currentTeam }
+    return {teams, fetchById, currentTeam}
 })
